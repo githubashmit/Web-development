8a13f7069fa3eacc255891ea7d41266080b87e64
Ashmit <ashmitgithub@gmail.com>
2022-12-02 16:51:49 +0530

    02.12.2022

---
+++ Javascript/func.js
@@ -0,0 +1,19 @@
+console.log("This is function in javascript");
+
+function greet(name , greettext){
+    console.log(greettext+" " + name)
+    console.log(name+" is a good boy");
+}
+let name ="harry";
+let name1 ="Shubham"
+let name2 ="Ashmit";
+let name3 ="Sagar";
+let greettext="good morning";
+greet(name,greettext);
+greet(name1,greettext);
+greet(name2,greettext);
+greet(name3,greettext);
+// console.log(name+" is a good boy");
+// console.log(name1+" is a good boy");
+// console.log(name2+" is a good boy");
+// console.log(name3+" is a good boy");
